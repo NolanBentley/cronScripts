bc82c2bc372a5b09da8bb3449d95d3fe4a120256
NolanBentley <NolanBentley@gmail.com>
2024-07-29 11:53:36 -0500

    init commit of cron scripts

---
+++ checkIpLog.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+##Indicate usage to nolan
+currTime=$(date "+%Y/%m/%d @ %h:%M:%S %r")
+currTime2=$(date "+%Y-%m-%d_%H-%M-%S")
+log1=/home/nolan/cronLog.txt
+echo "cron diskInfo script run at $currTime" >> $log1
+log2=$(tail -n 100 $log1)
+echo $log2 > $log1
+
+
+## Save and trim ip logs
+cd /home/SharedUser/ipLog/
+cat ipLog.txt > ipLog_prev
+tail -n 20 ipLog.txt  > ipLog_tail
+cat ipLog_tail | awk '!x[$0]++' > ipLog.txt
+
+## Add new ip's to the compiled file
+cat ipList > ipList_prev
+cat ipLog.txt >> ipList
+cat ipList | sort | awk '!x[$0]++' > ipList_new
+cat ipList_new > ipList
+
+#Email me if there is a new login
+ogLEN1=$(wc -l ipList | cut -f 1 -d " ")
+ogLEN2=$(wc -l ipList_prev| cut -f 1 -d " ")
+if [ "$ogLEN1" != "$ogLEN2" ] ; then
+	echo "#Diff of ip lists at $currTime:" > message_$currTime2
+	diff ipList ipList_prev >> message_$currTime2
+	echo "" >> message_$currTime2
+	echo "#Current ipList:" >> message_$currTime2
+	cat ipList >> message_$currTime2
+        cat message_$currTime2 | mutt -s "Low urgency: New ip address on popgen3" -F /home/nolan/.muttrc -- "nolanbentley@utexas.edu"
+fi
+
---
+++ diskInfo.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+##Indicate usage to nolan
+currTime=$(date "+%Y/%m/%d @ %h:%M:%S %r")
+currTime2=$(date "+%Y-%m-%d_%H-%M-%S")
+log1=/home/nolan/cronLog.txt
+echo "cron diskInfo script run at $currTime" >> $log1
+log2=$(tail -n 100 $log1)
+echo $log2 > $log1
+
+## Save current disk info
+echo "Report on " $currTime > /opt/MegaRAID/perccli/diskInfo.txt
+/opt/MegaRAID/perccli/perccli64 /c0 /eall /sall show all >> /opt/MegaRAID/perccli/diskInfo.txt
+
+## Save current to end of history
+tail -100000 /opt/MegaRAID/perccli/diskInfoHistory.txt > /opt/MegaRAID/perccli/diskInfoHistory.txt
+cat /opt/MegaRAID/perccli/diskInfo.txt >> /opt/MegaRAID/perccli/diskInfoHistory.txt
+echo "" >> /opt/MegaRAID/perccli/diskInfoHistory.txt
+echo "" >> /opt/MegaRAID/perccli/diskInfoHistory.txt
+
+## Save a summary 
+date "+%Y/%m/%d @ %h:%M:%S %r" > /opt/MegaRAID/perccli/diskInfoSummary.txt
+grep -E "S.M.A.R.T.|Error|HDD" /opt/MegaRAID/perccli/diskInfo.txt > /opt/MegaRAID/perccli/diskInfoSummary.txt
+
+# Make a report
+echo "Report on: " $currTime > /opt/MegaRAID/perccli/diskReport.txt
+cat /opt/MegaRAID/perccli/diskInfoSummary.txt >> /opt/MegaRAID/perccli/diskReport.txt
+echo "" >> /opt/MegaRAID/perccli/diskReport.txt
+echo "########### Full Report #############" >> /opt/MegaRAID/perccli/diskReport.txt
+cat /opt/MegaRAID/perccli/diskInfo.txt >> /opt/MegaRAID/perccli/diskReport.txt
+
+###### Check for changes to SMART! #######
+## Analyze summary files and if ogLEN1!=ogLEN2+1, then email alert
+ogLEN1=$(egrep "^S\.M\.A\.R\.T alert" /opt/MegaRAID/perccli/diskInfoSummary.txt | wc -l |cut -f 1 -d " ")
+ogLEN2=$(egrep "^S\.M\.A\.R\.T alert.* = No$" /opt/MegaRAID/perccli/diskInfoSummary.txt | wc -l | cut -f 1 -d " ")
+if [ "$ogLEN1" != "$ogLEN2" ] ; then
+	message=/opt/MegaRAID/perccli/smartBasedMessage_$currTime2
+        echo "####### SMART report ########" > $message
+	echo $ogLEN1 >> $message
+	echo $ogLEN2 >> $message
+	cat /opt/MegaRAID/perccli/diskReport.txt >> $message
+	cat $message | mutt -s "URGENT: Alert regarding Popgen3's RAID drives" -F /home/nolan/.muttrc -- "nolanbentley@utexas.edu, tjuenger@austin.utexas.edu,li.zhang@austin.utexas.edu ,jebonnette@utexas.edu"
+fi
+
+###### Email admins weekly and head admin daily ########
+day=$(date "+%a")
+dayToCheck="Fri"
+if [ $day = $dayToCheck ] ; then
+	cat /opt/MegaRAID/perccli/diskReport.txt | mutt -s "Weekly report regarding Popgen3's RAID drives" -F /home/nolan/.muttrc -- "nolanbentley@utexas.edu,li.zhang@austin.utexas.edu ,jebonnette@utexas.edu"
+else
+	cat /opt/MegaRAID/perccli/diskReport.txt | mutt -s "Daily report regarding Popgen3's RAID drives" nolanbentley@gmail.com -F /home/nolan/.muttrc
+fi
+
+#Make a copy of current disk summary
+diffOut=$(diff /opt/MegaRAID/perccli/diskInfoSummary_prev.txt /opt/MegaRAID/perccli/diskInfoSummary.txt)
+diffWc=$(diff /opt/MegaRAID/perccli/diskInfoSummary_prev.txt /opt/MegaRAID/perccli/diskInfoSummary.txt | wc -l | cut -f 1 -d " ")
+if [ "$diffWc" != 0 ] ; then
+	message=/opt/MegaRAID/perccli/message_$currTime2
+	echo "####### Diff report ########" >> $message
+	echo $diffOut >> $message
+	echo "############################" >> $message
+	echo "" >> $message
+	echo "####### Full report ########" >> $message
+	cat /opt/MegaRAID/perccli/diskReport.txt >> $message
+	cat $message | mutt -s "URGENT: Something has changed regarding popgen3's RAID drives " -F /home/nolan/.muttrc -- "nolanbentley@utexas.edu, tjuenger@austin.utexas.edu,li.zhang@austin.utexas.edu ,jebonnette@utexas.edu"
+fi
+
+#Make a copy of current disk summary the previous
+cp /opt/MegaRAID/perccli/diskInfoSummary.txt /opt/MegaRAID/perccli/diskInfoSummary_prev.txt
+
+#else exit
+exit 0
