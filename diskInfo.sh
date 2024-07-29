03ff381215a8b007a2a3ed22c6c0178e699b5cd3
NolanBentley <NolanBentley@gmail.com>
2024-07-29 12:11:08 -0500

    Modified the disk info files to be variable based

--- diskInfo.sh
+++ diskInfo.sh
@@ -6,6 +6,8 @@ currTime=$(date "+%Y/%m/%d @ %h:%M:%S %r")
 currTime2=$(date "+%Y-%m-%d_%H-%M-%S")
 log1=/home/nolan/cronLog.txt
 ipLogLoc=/home/SharedUser/ipLog/
+diskInfo=/opt/MegaRAID/perccli/diskInfo.txt
+diskHist=/opt/MegaRAID/perccli/diskInfoHistory.txt
 
 ##Update log
 echo "cron $scriptName run at $currTime" >> $log1
@@ -13,12 +15,14 @@ log2=$(tail -n 100 $log1)
 echo $log2 > $log1
 
 ## Save current disk info
-echo "Report on " $currTime > /opt/MegaRAID/perccli/diskInfo.txt
-/opt/MegaRAID/perccli/perccli64 /c0 /eall /sall show all >> /opt/MegaRAID/perccli/diskInfo.txt
+echo "Report on " $currTime > $diskInfo
+/opt/MegaRAID/perccli/perccli64 /c0 /eall /sall show all >> $diskInfo
 
 ## Save current to end of history
-tail -100000 /opt/MegaRAID/perccli/diskInfoHistory.txt > /opt/MegaRAID/perccli/diskInfoHistory.txt
-cat /opt/MegaRAID/perccli/diskInfo.txt >> /opt/MegaRAID/perccli/diskInfoHistory.txt
+histTxt=$(tail -100000 $diskHist)
+echo $histTxt > $diskHist
+echo "##########" >> $diskHist
+cat $diskInfo >> $diskHist
 echo "" >> /opt/MegaRAID/perccli/diskInfoHistory.txt
 echo "" >> /opt/MegaRAID/perccli/diskInfoHistory.txt
 
@@ -52,10 +56,17 @@ dayToCheck="Fri"
 if [ $day = $dayToCheck ] ; then
 	cat /opt/MegaRAID/perccli/diskReport.txt | mutt -s "Weekly report regarding Popgen3's RAID drives" -F /home/nolan/.muttrc -- "nolanbentley@utexas.edu,li.zhang@austin.utexas.edu ,jebonnette@utexas.edu"
 else
+        message=/opt/MegaRAID/perccli/dailyMessage.txt
+        echo "####### Diff report ########" >> $message
+        diff 
+        echo "############################" >> $message
+        echo "" >> $message
+        echo "####### Full report ########" >> $message
+        cat /opt/MegaRAID/perccli/diskReport.txt >> $message
 	cat /opt/MegaRAID/perccli/diskReport.txt | mutt -s "Daily report regarding Popgen3's RAID drives" nolanbentley@gmail.com -F /home/nolan/.muttrc
 fi
 
-#Make a copy of current disk summary
+#Check for differences in the summary text
 diffOut=$(diff /opt/MegaRAID/perccli/diskInfoSummary_prev.txt /opt/MegaRAID/perccli/diskInfoSummary.txt)
 diffWc=$(diff /opt/MegaRAID/perccli/diskInfoSummary_prev.txt /opt/MegaRAID/perccli/diskInfoSummary.txt | wc -l | cut -f 1 -d " ")
 if [ "$diffWc" != 0 ] ; then
@@ -71,6 +82,7 @@ fi
 
 #Make a copy of current disk summary the previous
 cp /opt/MegaRAID/perccli/diskInfoSummary.txt /opt/MegaRAID/perccli/diskInfoSummary_prev.txt
+cp /opt/MegaRAID/perccli/diskReport.txt /opt/MegaRAID/perccli/diskReport_prev.txt
 
 #else exit
 exit 0
